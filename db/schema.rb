68cb04d43fa3f391e703ddf1c24484e0fe27102d
Anastasia <a.g.feygina@gmail.com>
2014-12-12 11:06:04 +0300

    Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208195637) do
+ActiveRecord::Schema.define(version: 20141211215835) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.string   "title"
