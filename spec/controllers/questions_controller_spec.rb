ea067dcc70d4b081b0f0be8e51695838e96514e0
Anastasia <a.g.feygina@gmail.com>
2014-12-14 13:18:51 +0300

    Chanded spec with show method

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe QuestionsController, :type => :controller do
   describe 'GET #index' do
     let(:questions) { FactoryGirl.create_list(:question, 2) }
-    
-    before do
-      get :index
-    end
+
+    before { get :index }
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(@questions)
@@ -14,6 +12,20 @@ RSpec.describe QuestionsController, :type => :controller do
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { FactoryGirl.create(:question) }
+
+    before {get :show, id: question
+    }
+    it 'assings the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
