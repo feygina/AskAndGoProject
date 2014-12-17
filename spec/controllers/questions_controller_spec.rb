38d2b155fed9d89b26905089016652b8d79d8a34
Anastasia <a.g.feygina@gmail.com>
2014-12-17 22:35:28 +0300

    Dont save invalid questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
+  let(:question) { FactoryGirl.create(:question) }
+
   describe 'GET #index' do
     let(:questions) { FactoryGirl.create_list(:question, 2) }
 
@@ -16,8 +18,6 @@ RSpec.describe QuestionsController, :type => :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { FactoryGirl.create(:question) }
-
     before {get :show, id: question
     }
     it 'assings the requested question to @question' do
@@ -42,7 +42,6 @@ RSpec.describe QuestionsController, :type => :controller do
   end
 
   describe 'GET #edit' do
-    let(:question) { FactoryGirl.create(:question) }
     before { get :edit, id: question }
 
     it 'assings the requested question to @question' do
@@ -51,6 +50,30 @@ RSpec.describe QuestionsController, :type => :controller do
 
     it 'renders edit view' do
       expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question in the database' do
+        expect { post :create, question: FactoryGirl.attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, question: FactoryGirl.attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, question: FactoryGirl.attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question: FactoryGirl.attributes_for(:invalid_question)
+        expect(response).to render_template :new
+        end
     end
   end
 end
