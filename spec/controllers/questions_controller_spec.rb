94aa45b8e2b1736fe8e83ece5ac7a57754d80435
Anastasia <a.g.feygina@gmail.com>
2014-12-18 14:05:48 +0300

    Question controller with update and delite methods

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -76,4 +76,46 @@ RSpec.describe QuestionsController, :type => :controller do
         end
     end
   end
+
+  describe 'PATCH #update' do
+    context 'valid attributes' do
+      it 'assings the requested question to @question' do
+        patch :update, id: question, question: FactoryGirl.attributes_for(:question)
+        expect(assigns(:question)).to eq question
+      end
+      it 'changes question attributes' do
+        patch :update, id: question, question: { title: 'new title', body: 'new body'}
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+      it 'redirects to the updated question' do
+        patch :update, id: question, question: FactoryGirl.attributes_for(:question)
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'invalid attributes' do
+      before { patch :update, id: question, question: { title: 'new title', body: nil} }
+      it 'does not change question attributes' do
+        question.reload
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'
+      end
+      it 're-renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before { question }
+    it 'deletes question' do
+      expect { delete :destroy, id: question }.to change(Question, :count).by(-1)
+    end
+    it 'redirect to index view' do
+      delete :destroy, id: question
+      expect(response).to redirect_to questions_path
+    end
+  end
 end
