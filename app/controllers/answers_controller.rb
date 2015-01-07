d44ff1a317c3751c5073bcaac4f44fd3dc7e1eec
Anastasia <a.g.feygina@gmail.com>
2015-01-07 17:21:07 +0300

    added new to Acontroller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,9 @@
 class AnswersController < ApplicationController
+  before_action :load_question, only: [:index, :create, :update]
+  before_filter :require_login, only: [:create, :destroy, :update]
+
+  def new
+    @answer = @question.build
+  end
+
 end
